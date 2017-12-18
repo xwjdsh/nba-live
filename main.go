f42f4da14cbcfe4a8ca7c7bd2f2856c6bcee4adf
Wendell Sun <iwendellsun@gmail.com>
2017-12-18 15:00:32 +0800

    Delete test code

--- main.go
+++ main.go
@@ -21,29 +21,6 @@ func main() {
 		return
 	}
 
-	//games := []*Game{
-	//&Game{
-	//HomeTeam:   "骑士",
-	//VisitTeam:  "勇士",
-	//HomeScore:  "100",
-	//VisitScore: "90",
-	//PeriodCn:   "第4节 01:30",
-	//},
-	//&Game{
-	//HomeTeam:   "凯尔特人",
-	//VisitTeam:  "公牛",
-	//HomeScore:  "80",
-	//VisitScore: "88",
-	//PeriodCn:   "第3节 03:30",
-	//},
-	//&Game{
-	//HomeTeam:   "火箭",
-	//VisitTeam:  "活塞",
-	//HomeScore:  "120",
-	//VisitScore: "99",
-	//PeriodCn:   "第4节 05:30",
-	//},
-	//}
 	i, err := newSelect(games)
 	if err != nil {
 		fmt.Printf("%s %s\n", Red(IconBad), "选择比赛错误")
